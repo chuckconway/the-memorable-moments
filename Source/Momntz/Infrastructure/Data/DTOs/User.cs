eb18a32cd363429e35eba081fea9c18bdeea0b82
Chuck Conway <chuckconway@users.noreply.github.com>
2011-12-09 23:32:02 -0800

    Missing files

---
+++ Source/Momntz/Infrastructure/Data/DTOs/User.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Momntz.Infrastructure.Data.DTOs
+{
+    public class User : IPrimaryKey<int>
+    {
+        /// <summary>
+        /// Gets or sets the id.
+        /// </summary>
+        /// <value>
+        /// The id.
+        /// </value>
+        public virtual int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the first name.
+        /// </summary>
+        /// <value>
+        /// The first name.
+        /// </value>
+        public virtual string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last name.
+        /// </summary>
+        /// <value>
+        /// The last name.
+        /// </value>
+        public virtual string LastName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the username.
+        /// </summary>
+        /// <value>
+        /// The username.
+        /// </value>
+        public virtual string Username { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email.
+        /// </summary>
+        /// <value>
+        /// The email.
+        /// </value>
+        public virtual string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the password.
+        /// </summary>
+        /// <value>
+        /// The password.
+        /// </value>
+        public virtual string Password { get; set; }
+
+        /// <summary>
+        /// Gets or sets the create date.
+        /// </summary>
+        /// <value>
+        /// The create date.
+        /// </value>
+        public virtual DateTime CreateDate { get; set; }
+    }
+}
